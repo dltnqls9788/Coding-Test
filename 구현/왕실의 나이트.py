ad36d321b800f00ecdc0fc6e41db9d25d5ea93ed
Su Bin <dltnqls9788@naver.com>
2022-09-11 23:42:03 +0900

    0911

---
+++ 구현/왕실의 나이트.py
@@ -0,0 +1,17 @@
+# 현재 나이트 위치 입력받기
+input_data = input()
+row = int(input_data[1])
+col = int(ord(input_data[0])) - int(ord('a')) + 1 
+
+# 나이트가 이동할 수 있는 8가지 방향 정의
+steps = [(2,-1), (2,1), (-2,1), (-2,-1), (-1,2), (1,2), (-1,-2), (1,-2)]
+
+cnt = 0 
+for step in steps:
+    next_row = row + step[0]
+    next_col = col + step[1]
+
+    if next_row >=1 and next_row <=8 and next_col >=1 and next_col <=8:
+        cnt += 1
+    
+print(cnt)
